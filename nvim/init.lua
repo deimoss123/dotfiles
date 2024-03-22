5c46b336a4a71a91c30b009285e20c16aec88c9e
daniels <danielsalksnis@gmail.com>
2024-03-22 02:47:45 +0200

    noņemt lieku rindiņu

--- nvim/init.lua
+++ nvim/init.lua
@@ -1313,7 +1313,6 @@ require('lazy').setup {
 
       -- Only one of these is needed, not both.
       'nvim-telescope/telescope.nvim', -- optional
-      'ibhagwan/fzf-lua', -- optional
     },
     config = true,
   },
